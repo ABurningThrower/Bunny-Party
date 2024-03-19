a23657276c8db9a4e2a009a7216ba80795bab329
ABurningThrower <ABurningThrower>
2024-03-19 13:19:30 -0400

    init 2.0

---
+++ files/Scenes/main_menu.gd
@@ -0,0 +1,19 @@
+extends TextureRect
+
+
+
+
+func _on_local_btn_pressed():
+	pass
+
+
+func _on_online_btn_pressed():
+	pass
+
+
+func _on_settings_btn_pressed():
+	pass # go to settings
+
+
+func _on_exit_btn_pressed():
+	pass # close window
